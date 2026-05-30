ae3edcd21589ed92f231ea8d665f4ff5eef7e205
PaimanUwU <adiaimanputra.work@gmail.com>
2026-05-30 16:18:22 +0800

    feat(Frontend): add layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Submit from './pages/Submit'
+import { MainLayout } from './layouts/MainLayout'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/submit" element={<Submit />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/submit" element={<Submit />} />
+        </Route>
       </Routes>
     </Router>
   )
